ef3910683de8b11f494cf1db855034a69ee83273
Tyler McCracken <tylermmccracken@gmail.com>
2015-04-02 17:27:06 -0400

    Changing to masked array sturcture  for fiber, need to fix though

--- ftt_centroid.py
+++ ftt_centroid.py
@@ -8,21 +8,20 @@ Created on Wed Apr 01 11:16:10 2015
 import matplotlib.pyplot as plt
 import numpy as np
 import plotRoutine
+from scipy.stats import norm
+from numpy import ma
 
-field = 15.0  # [arcsecs]
+field = 10.0  # [arcsecs]
 pixel_size = 10.0  # [arcsecs] 
 grid_size = 1000.  # points on the grid
-#field_scale = np.linspace(-field/2.0, field/2.0, grid)
-#field_grid = np.ones((grid, grid))
 
 """
 Specifying the spot size and fiber size. Fiber sized to 1/e (37% of peak) or 1/e2 (13.5% of peak).
 These values are beam radius though, need diameter.
 TODO: calculate 1/e diameter
 """
-fwhm = 2.0  # fwhm of beam [arcsecs]
-fiber_size = 2 * 0.8493218 * fwhm  # 1/e^2 DIAMETER [arcsecs]
-#beam_diameter = 0.37  # 1/e DIAMETER TODO
+fwhm = 0.76  # fwhm of beam [arcsecs]
+fiber_radius = 0.45  # 1/e radius [arcsecs]
 sigma = fwhm / 2.3458  # [arcsecs]
 mu = 0.0  # [arcsecs]
 
@@ -33,20 +32,41 @@ Set up distributions to for binning.
 magnitude = 0.0  # TODO
 n_photons = 10000.0
 
-x = np.random.normal(0.0, sigma, n_photons)
-y = np.random.normal(0.0, sigma, n_photons)
+# Generate random photons
+x = norm.rvs(loc=0.0, scale=sigma, size=n_photons)
+y = norm.rvs(loc=0.0, scale=sigma, size=n_photons)
+
+# Mask for fiber
+x_masked = ma.masked_inside(x, -fiber_radius, fiber_radius).compressed()
+y_masked = ma.masked_inside(y, -fiber_radius, fiber_radius).compressed()
+
+# Adjust length for histogram
+x = np.zeros(n_photons)
+x[0:len(x_masked)] = x_masked
+y=np.zeros(n_photons)
+y[0:len(y_masked)] = y_masked
+
 
 """
-Fine binning, take out fiber
+Bin for detector
 """
-n_bins = 5000
-assert np.mod(n_bins, 2) == 0  # Make fine binning even
-bin_size = field / n_bins  # [arcsecs]
-mask = plotRoutine.fiber_mask(fiber_size, n_bins, bin_size)
-data, x1, y2 = plotRoutine.binner(x, y, n_bins, bin_size)
+n_bins = 500
+bin_size = field / n_bins
+xlims = [-(n_bins/2.0 * bin_size), (n_bins/2.0 * bin_size)]
+ylims = [-(n_bins/2.0 * bin_size), (n_bins/2.0 * bin_size)]
+
+# Create histogram and kill center value used for mathcing arrays
+H, xedges, yedges = np.histogram2d(x, y, bins=n_bins, range=[xlims, ylims])
+H[n_bins/2, n_bins/2] = 0 
+
 
 #plt.imshow(data)
-plt.imshow(mask * data, interpolation='none')
+#plt.figure()
+plt.imshow(H, interpolation='none')
 
 
-#plotRoutine.plotter(x, y, pixel_size, grid_size, fiber_size)
+
+'''
+mask = plotRoutine.fiber_mask(fiber_radius, n_bins, bin_size)
+data, x1, y2 = plotRoutine.binner(x, y, n_bins, bin_size)
+'''
