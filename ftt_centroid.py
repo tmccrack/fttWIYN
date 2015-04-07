464ee6520e672f6adc31dcd814c0b2684c4fdf2f
Tyler McCracken <tylermmccracken@gmail.com>
2015-04-07 14:03:44 -0400

    Changed number of bins to better adapt to pixel size

--- ftt_centroid.py
+++ ftt_centroid.py
@@ -16,12 +16,12 @@ from numpy import ma
 Instrument and source properties
 """
 #magnitude = np.arange(10.0, 16.0)  # TODO
-magnitude = 10.0
+magnitude = 16.0
 diameter = 3.5  # [m]
 wavelength = 635  # [nm]
 bandpass = 880 - 390  # [nm]
 extinction = 0.5  # Extinction in magnitudes per airmass
-transmission = 0.72
+transmission = 0.53
 qe = 0.9
 fiber_rejection = 0.16
 integration_time = 0.01  # [seconds]
@@ -32,54 +32,57 @@ integration_time = 0.01  # [seconds]
 """
 Detector and fiber properties
 """
-field = 10.0  # [arcsecs]
-pixel_size = np.array([5.0, 2.5, 2.0, 1.0, 0.5])  # [arcsecs] 
+field = 5.0  # [arcsecs]
+#pixel_size = np.arange(0.01, 0.07, 0.01) # [arcsecs] 
+pixel_size = np.array([0.01, 0.02, 0.03333333, 0.04, 0.05, 0.06666667])
 fwhm = 0.76  # fwhm of beam [arcsecs]
 fiber_radius = 0.45  # 1/e radius [arcsecs]
 sigma = fwhm / 2.3458  # [arcsecs]
 mux = 0.05  # [arcsecs]
-muy = 0.0  # [arcsecs]
+muy = 0.05  # [arcsecs]
+em_gain = 500.
 noise_level = 0
 
-
-"""
-Fiber mask, binning parameters
-"""
-n_bins = 500  # Grid to be binned into detector
-bin_size = field / n_bins
-mask = plotRoutine.fiber_mask(fiber_radius, n_bins, bin_size)
-xlims = [-(n_bins/2.0 * bin_size), (n_bins/2.0 * bin_size)]
-ylims = [-(n_bins/2.0 * bin_size), (n_bins/2.0 * bin_size)]
-
-
-# Pixel edges on detector
-xedges = np.linspace(xlims[0], xlims[1], n_bins+1)
-yedges = np.linspace(xlims[0], xlims[1], n_bins+1)
-
-
+realizations = 100
 
 """
 Realizations
 """
 err = np.zeros((len(pixel_size), 2))
-for m in range(len(pixel_size)):
-    # Do not take fiber rejection, taken up in mask
+for m in range(len(pixel_size)):        
+    """
+    Fiber mask, binning parameters
+    """
+    n_bins = np.int(30 * field /pixel_size[m])  # Grid to be binned into detector
+    if (n_bins%2 != 0):
+        n_bins += 1  # n_bins must be even
+    bin_size = field / n_bins
+    mask = plotRoutine.fiber_mask(fiber_radius, n_bins, bin_size)
+    xlims = [-(n_bins/2.0 * bin_size), (n_bins/2.0 * bin_size)]
+    ylims = [-(n_bins/2.0 * bin_size), (n_bins/2.0 * bin_size)]
+    
+    # Pixel edges on detector
+    xedges = np.linspace(xlims[0], xlims[1], n_bins+1)
+    yedges = np.linspace(xlims[0], xlims[1], n_bins+1)    
+    
+    
+    # Do not consider fiber rejection, taken up in mask
     n_photons = plotRoutine.source_photons(bandpass, wavelength, magnitude, diameter, extinction) * qe * transmission * integration_time    
     
-    n_pix = field / pixel_size[m]  # Detector pixels
+    n_pix = np.round(field / pixel_size[m])  # Detector pixels
     bins_per_pix = n_bins / n_pix
     xdet_edges = xedges[::bins_per_pix]  # [arcsecs]
     ydet_edges = yedges[::bins_per_pix]  # [arcsecs]
-    err2 = np.zeros(1000)
+    err2 = np.zeros(realizations)
+    print(m)    
     
-    for n in range(1000):
+    for n in range(realizations):
         # Generate random photons
         # Dominate error in EMCCDs is multiplicative error in gain stage
         # Gain factor adds sqrt(2) to the shot noise
         # Accomodate by multiplying n_photons by sqrt(2)
-        gain_noise_photons = n_photons + norm.rvs(loc=0.0, scale=np.sqrt(2), size=1)
-        x = norm.rvs(loc=mux, scale=sigma, size=gain_noise_photons)
-        y = norm.rvs(loc=muy, scale=sigma, size=gain_noise_photons)
+        x = norm.rvs(loc=mux, scale=sigma, size=n_photons*em_gain)
+        y = norm.rvs(loc=muy, scale=sigma, size=n_photons*em_gain)
         # Histogram of photons on focal plane, apply fiber mask, bin for detector
         H, xedges, yedges = np.histogram2d(x, y, bins=n_bins, range=[xlims, ylims])
         H = H * mask
@@ -94,6 +97,7 @@ for m in range(len(pixel_size)):
         xc += pixel_size[m] / 2.0
         yc += pixel_size[m] / 2.0
         err2[n] = np.sqrt((xc-mux)**2 + (yc-muy)**2)
+        print('\t' + str(n))
         
     err[m,:] = [np.std(err2), np.mean(err2)]
 
