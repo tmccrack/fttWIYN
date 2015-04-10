4377844fd94855121a589f215ffc077b02fc2d75
Tyler McCracken <tylermmccracken@gmail.com>
2015-04-10 17:06:58 -0400

    Updated realizations

--- ftt_centroid.py
+++ ftt_centroid.py
@@ -21,7 +21,7 @@ diameter = 3.5  # [m]
 wavelength = 635  # [nm]
 bandpass = 880 - 390  # [nm]
 extinction = 0.5  # Extinction in magnitudes per airmass
-transmission = 0.53
+transmission = 0.523
 qe = 0.9
 fiber_rejection = 0.16
 integration_time = 0.01  # [seconds]
@@ -33,17 +33,26 @@ integration_time = 0.01  # [seconds]
 Detector and fiber properties
 """
 field = 5.0  # [arcsecs]
-#pixel_size = np.arange(0.01, 0.07, 0.01) # [arcsecs] 
-pixel_size = np.array([0.01, 0.02, 0.03333333, 0.04, 0.05, 0.06666667])
+#
+#pixel_size = np.array([1.0, 0.75, 0.5, 0.25, 0.2, 0.175, 0.15, 0.125, 0.1, 0.075, 0.05, 0.025])
+pixel_size = np.array([0.025])
+#
+n_pix = np.round(field / pixel_size)  # Pixels in ~5 arcsecond field
+n_bins = n_pix * 30.0  # Bins for putting photons on prior to detector binning
+bin_size = pixel_size / 30.0
+bins_per_pix = n_bins / n_pix  # Photon bins per detector pixel
+
 fwhm = 0.76  # fwhm of beam [arcsecs]
 fiber_radius = 0.45  # 1/e radius [arcsecs]
 sigma = fwhm / 2.3458  # [arcsecs]
-mux = 0.05  # [arcsecs]
-muy = 0.05  # [arcsecs]
-em_gain = 500.
-noise_level = 0
+mux = 0.0  # [arcsecs]
+muy = 0.0  # [arcsecs]
+em_gain = 1000.
+rn = 0.5  # read noise [electrons/pixel]
+dark = 0.001 * integration_time  # dark current [electrons/pixel]
+noise_level = rn+dark
 
-realizations = 100
+realizations = 1
 
 """
 Realizations
@@ -53,26 +62,20 @@ for m in range(len(pixel_size)):
     """
     Fiber mask, binning parameters
     """
-    n_bins = np.int(30 * field /pixel_size[m])  # Grid to be binned into detector
-    if (n_bins%2 != 0):
-        n_bins += 1  # n_bins must be even
-    bin_size = field / n_bins
-    mask = plotRoutine.fiber_mask(fiber_radius, n_bins, bin_size)
-    xlims = [-(n_bins/2.0 * bin_size), (n_bins/2.0 * bin_size)]
-    ylims = [-(n_bins/2.0 * bin_size), (n_bins/2.0 * bin_size)]
+      # Grid to be binned into detector
+    mask = plotRoutine.fiber_mask(fiber_radius, n_bins[m], bin_size[m])
+    lim = n_bins[m]/2.0 * bin_size[m]  # edge of bin grid in arcseconds
     
     # Pixel edges on detector
-    xedges = np.linspace(xlims[0], xlims[1], n_bins+1)
-    yedges = np.linspace(xlims[0], xlims[1], n_bins+1)    
+    xedges = np.linspace(-lim, lim, n_bins[m]+1)
+    yedges = np.linspace(-lim, lim, n_bins[m]+1)    
     
     
     # Do not consider fiber rejection, taken up in mask
     n_photons = plotRoutine.source_photons(bandpass, wavelength, magnitude, diameter, extinction) * qe * transmission * integration_time    
     
-    n_pix = np.round(field / pixel_size[m])  # Detector pixels
-    bins_per_pix = n_bins / n_pix
-    xdet_edges = xedges[::bins_per_pix]  # [arcsecs]
-    ydet_edges = yedges[::bins_per_pix]  # [arcsecs]
+    xdet_edges = xedges[::bins_per_pix[m]]  # [arcsecs]
+    ydet_edges = yedges[::bins_per_pix[m]]  # [arcsecs]
     err2 = np.zeros(realizations)
     print(m)    
     
@@ -81,13 +84,13 @@ for m in range(len(pixel_size)):
         # Dominate error in EMCCDs is multiplicative error in gain stage
         # Gain factor adds sqrt(2) to the shot noise
         # Accomodate by multiplying n_photons by sqrt(2)
-        x = norm.rvs(loc=mux, scale=sigma, size=n_photons*em_gain)
-        y = norm.rvs(loc=muy, scale=sigma, size=n_photons*em_gain)
+        x = norm.rvs(loc=mux, scale=sigma, size=np.int(n_photons*em_gain))
+        y = norm.rvs(loc=muy, scale=sigma, size=np.int(n_photons*em_gain))
         # Histogram of photons on focal plane, apply fiber mask, bin for detector
-        H, xedges, yedges = np.histogram2d(x, y, bins=n_bins, range=[xlims, ylims])
+        H, xedges, yedges = np.histogram2d(x, y, bins=n_bins[m], range=[[-lim, lim], [-lim, lim]])
         H = H * mask
-        detector = plotRoutine.detector_bin(H, n_pix) + plotRoutine.add_gaussian_noise(n_pix, noise_level)
-    
+        detector = plotRoutine.detector_bin(H, n_pix[m]) + plotRoutine.add_gaussian_noise(n_pix[m], noise_level)
+        
         """
         Centroid and error
         """
@@ -100,5 +103,9 @@ for m in range(len(pixel_size)):
         print('\t' + str(n))
         
     err[m,:] = [np.std(err2), np.mean(err2)]
+    #figure()
+    #plt.imshow(detector, interpolation='none')
+    #plt.colorbar()
 
+figure()
 plt.plot(pixel_size, err[:,1])
